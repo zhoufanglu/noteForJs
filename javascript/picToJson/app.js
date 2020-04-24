fee33edf6a73711ceef85979b41b35cbfa408056
卢方舟 <lufangzhou@adinnet.cn>
2020-04-24 16:43:02 +0800

    add some

---
+++ javascript/picToJson/app.js
@@ -0,0 +1,13 @@
+//图片table文档转json
+node_xj = require("xls-to-json");
+node_xj({
+    input: "test.xls",  // input xls
+    output: "package.json" // output json
+    //sheet: "sheet1",  // specific sheetname
+}, function(err, result) {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log(result);
+    }
+});
