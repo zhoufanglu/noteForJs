70a5632ae37f57e6946866a64ed37308c256bd18
卢方舟 <lufangzhou@adinnet.cn>
2020-04-27 09:45:58 +0800

    study little red book

---
+++ javascript/getImgName/getImgName.js
@@ -0,0 +1,44 @@
+//获取项目工程里的图片
+var fs = require('fs');//引用文件系统模块
+function readFileList(path, filesList) {
+    var files = fs.readdirSync(path);
+    files.forEach(function (itm, index) {
+        var stat = fs.statSync(path + itm);
+        if (stat.isDirectory()) {
+            //递归读取文件
+            readFileList(path + itm + "/", filesList)
+        } else {
+            var obj = {};//定义一个对象存放文件的路径和名字
+            obj.path = path;//路径
+            obj.filename = itm//名字
+            filesList.push(obj);
+        }
+    })
+}
+var getFiles = {
+    //获取文件夹下的所有文件
+    getFileList: function (path) {
+        var filesList = [];
+        readFileList(path, filesList);
+        return filesList;
+    },
+    //获取文件夹下的所有图片
+    getImageFiles: function (path) {
+        var fileList = [];
+        this.getFileList(path).forEach((item) => {
+            fileList.push(item.filename)
+        });
+        return fileList;
+    }
+};
+//获取文件夹下的所有图片信息
+var imglist=getFiles.getImageFiles("./imgFile/");
+
+//1、清空文件数据
+fs.writeFile('./imgData.json', '', function(){console.log('清空成功')})
+//2、把得到的图片对象抛到imgData.json文件中
+fs.appendFile('./imgData.json', JSON.stringify(imglist), (err) => {
+    if (err) throw err;
+    console.log('数据已追加到文件');
+});
+
