518240a6103bab1754d321f4719ec59af6d89af4
zhoufanglu <735417031@qq.com>
2020-07-15 09:01:22 +0800

    study some ts

--- javascript/typescript/hello.ts
+++ javascript/typescript/hello.ts
@@ -1,4 +1,4 @@
-function sayHello(person: string) {
+/*function sayHello(person: string) {
     return 'Hello, ' + person;
 }
 
@@ -6,4 +6,19 @@ let user: string = '123';
 console.log(sayHello(user))
 
 let val = 'name'
-console.log(9, val)
+console.log(9, val)*/
+/**
+ * 数组
+ */
+//let fibonacci: number[] = [1, 1, 2, 3, 5];
+/**********************数组泛型***********************/
+let fibonacci: Array<number> = [1, 1, 2, 3, 5];
+//它可以创建一个指定长度的数组，同时将每一项都填充一个默认值：
+function createCusArray<T>(length:number, val: T ):Array<T> {
+    let arr:T[] = []
+    for(let i=0;i<length;i++){
+        arr.push(val)
+    }
+    return arr
+}
+console.log(createCusArray(10, '1'))
