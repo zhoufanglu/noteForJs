19ce795f0d49c4ae557b5994f9f0d4eec10fe2c9
卢方舟 <735417031@qq.com>
2020-12-04 14:13:39 +0800

    增加ip测试脚本

--- javascript/getImgName/testIP.js
+++ javascript/getImgName/testIP.js
@@ -1,2 +1,5 @@
-var address = require('address');
-console.log(address.ip())
+const address = require("address")
+const defaultGateway = require('default-gateway')
+const result = defaultGateway.v4.sync()
+const ip = address.ip(result && result.interface)
+console.log(ip) //ip函数不传参数时，和传入参数获取的ip值不一样，切传入参数后获取的 ip才是局域网可访问的。
